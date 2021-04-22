a6573ac9537fc6c93ff86213421ed157a1ddbeee
Alberto Vena <kennyadsl@gmail.com>
2021-04-22 10:22:57 +0200

    Use the correct Rails 6.1 method to retrieve application name
    It changed in Rails with rails/rails#34051 and the old method
    has been deprecated and removed. This code should work with both
    Rails 5.2 and 6.1+

--- lib/solidus_searchkick/engine.rb
+++ lib/solidus_searchkick/engine.rb
@@ -21,9 +21,10 @@ module SolidusSearchkick
       # If they have, then do not initialize searchkick on the model
       # If they have not, then set the defaults
       unless ::Spree::Product.try(:searchkick_options)
+        application_class_name = Rails.application.class.respond_to?(:module_parent) ? Rails.application.class.module_parent.name : Rails.application.class.parent.name
         ::Spree::Product.class_eval do
           searchkick(
-            index_name: "#{Rails.application.class.parent_name.parameterize.underscore}_spree_products_#{Rails.env}",
+            index_name: "#{application_class_name.parameterize.underscore}_spree_products_#{Rails.env}",
             word_start: [:name]
           )
         end
